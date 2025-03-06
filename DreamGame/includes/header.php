9d9a6b0d045d578f865b061475fa98186a85fab6
lean1000 <166569100+lean1000@users.noreply.github.com>
2025-03-06 00:08:42 -0300

    upload do pi
    upload do pi

---
+++ DreamGame/includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dream Game</title>
+    
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="./assets/img/logo/logo.png" alt="">
+
+            <ul>
+                <li><a href="#">loja</a></li>
+                <li><a href="#">destaques</a></li>
+                <li><a href="#">suporte</a></li>
+                <li><a href="#">sobre</a></li>
+            </ul>
+
+            <a href="#"><i class="bi bi-list"></i></a>
+
+            <a href="#"><img src="" alt=""></a>
+        </nav>
+    </header>
+
+
+
+
+
+
+
+    </body>
+</html>
