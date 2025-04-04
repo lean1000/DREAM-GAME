8600dbcb472802c5c26a98c48d8d1405555e73eb
Thbispo <thiagosouza0100@gmail.com>
2025-04-04 11:54:45 -0300

    Update destaques.php

--- DreamGame/destaques.php
+++ DreamGame/destaques.php
@@ -8,9 +8,8 @@
             <img src="./assets/img/capa dos produtos/Blackmyth_ Wu Kong.png" alt="Wukong">
 
             <div class="game-info">
+                <h4 class="titulo">Blackmyth Wukong</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
                     <span class="price">R$ 209,00</span>
                 </div>
 
@@ -23,15 +22,13 @@
         </div>
 
         <div class="card">
-            <img src="./assets/img/capa dos produtos/spider_man2_destaques.png" alt="Spider-Man 2">
-
+            <img src="./assets/img/capa dos produtos/spider_man2_destaques.png" alt="">
             <div class="game-info">
+            <h4 class="titulo">Spider-Man 2</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
-                    <span class="price">R$ 309,00</span>
-                </div>
 
+                    <span class="price">R$ 89,00</span>
+                </div>
                 <div class="logo">
                     <img src="./assets/img/logo/logo 1.png" alt="Dream Game">
                 </div>
@@ -42,12 +39,11 @@
 
         <div class="card">
             <img src="./assets/img/capa dos produtos/Star Wars Outlaws.png" alt="Star Wars Outlaws">
-
             <div class="game-info">
+            <h4 class="titulo">Star Wars Outlaws</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
-                    <span class="price">R$ 89,00</span>
+
+                    <span class="price">R$189,00</span>
                 </div>
 
                 <div class="logo">
@@ -59,12 +55,11 @@
         </div>
 
         <div class="card">
-            <img src="./assets/img/capa dos produtos/hogwarts.png" alt="Hogwarts Legacy">
+            <img src="./assets/img/capa dos produtos/hogwarts destaques.png" alt="Hogwarts Legacy">
 
             <div class="game-info">
+               <h4 class="titulo">Hogwarts Legacy</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
                     <span class="price">R$ 129,00</span>
                 </div>
 
@@ -80,9 +75,9 @@
             <img src="./assets/img/capa dos produtos/Mortal Kombat 1.png" alt="Mortal Kombat 1">
 
             <div class="game-info">
+                <h4 class="titulo">Mortal Kombat 1</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
+
                     <span class="price">R$ 109,00</span>
                 </div>
 
@@ -98,9 +93,9 @@
             <img src="./assets/img/capa dos produtos/Horizon-forbidden-west.png" alt="Horizon Forbidden West">
 
             <div class="game-info">
+                <h4 class="titulo">Horizon Forbidden </h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
+
                     <span class="price">R$ 139,00</span>
                 </div>
 
@@ -116,9 +111,8 @@
             <img src="./assets/img/capa dos produtos/forza 5 destaques.png" alt="Forza Horizon 5">
 
             <div class="game-info">
+                <h4 class="titulo">Forza Horizon 5</h4>
                 <div class="top-bar">
-                    <span class="destaques">destaques</span>
-                    <span class="info-icon">ℹ</span>
                     <span class="price">R$ 239,00</span>
                 </div>
 
